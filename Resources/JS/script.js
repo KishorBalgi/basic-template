fda45b599c7f43ec3f32dd5d45ea73dcf021abbe
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-06-22 20:35:35 +0530

    Mobile Nav

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -4,13 +4,17 @@ const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo-link");
 const navLinks = document.querySelector(".nav-links");
+const navMob = document.querySelector(".nav-mobile");
+const navMobIcon = document.querySelector(".nav-mobile img");
 const sections = document.querySelectorAll("section");
+let mobActive = 0;
 // SCROLL INTO VIEW:
 navLinks.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("nav-link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    if (mobActive) navMob.click();
   }
 });
 logo.addEventListener("click", function (e) {
@@ -47,3 +51,20 @@ const ObserverSection = new IntersectionObserver(sectionScroll, {
   threshold: 0.03,
 });
 sections.forEach((section) => ObserverSection.observe(section));
+// MOBILE NAV:
+navMob.addEventListener("click", function () {
+  logo.classList.toggle("hidden");
+  nav.classList.toggle("flex-column");
+  navLinks.classList.toggle("flex-column");
+  if (navMobIcon.getAttribute("src") === "Vendors/Icons/008-nav-icon.png") {
+    navMobIcon.setAttribute("src", "Vendors/Icons/009-nav-close.png");
+    navLinks.style.display = "flex";
+    nav.style.height = "fit-content";
+    mobActive = 1;
+  } else {
+    navMobIcon.setAttribute("src", "Vendors/Icons/008-nav-icon.png");
+    navLinks.style.display = "none";
+    nav.style.height = "55px";
+    mobActive = 0;
+  }
+});
